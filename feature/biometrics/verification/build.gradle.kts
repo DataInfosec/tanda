aa30ebf779f83b75a314206ee69bfab7efe3b3d5
conxtantyn <conxtantyn@gmail.com>
2026-07-21 10:37:14 +0200

    chore: fix verification module

--- feature/biometrics/verification/build.gradle.kts
+++ feature/biometrics/verification/build.gradle.kts
@@ -17,30 +17,30 @@ kotlin {
     }
     sourceSets {
         commonMain.dependencies {
-//            implementation(projects.feature.biometrics.domain)
-//
-//            implementation(libs.koin.core)
-//            implementation(libs.koin.annotation)
+            implementation(projects.feature.biometrics.domain)
+
+            implementation(libs.koin.core)
+            implementation(libs.koin.annotation)
         }
     }
 }
 
-//configurations {
-//    findByName("uniFfiConfiguration")?.let { remove(it) }
-//    create("uniFfiConfiguration") {
-//        isCanBeResolved = true
-//        isCanBeConsumed = false
-//    }
-//    matching { it.name.contains("RustRuntime") || it.name.contains("minGWX64") }
-//        .all {
-//            exclude(group = "Tenda.feature.biometrics", module = "domain")
-//        }
-//}
+configurations {
+    findByName("uniFfiConfiguration")?.let { remove(it) }
+    create("uniFfiConfiguration") {
+        isCanBeResolved = true
+        isCanBeConsumed = false
+    }
+    matching { it.name.contains("RustRuntime") || it.name.contains("minGWX64") }
+        .all {
+            exclude(group = "Tenda.feature.biometrics", module = "domain")
+        }
+}
 
-//tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
-//    dependsOn("buildUniffiBindings")
-//}
-//
-//tasks.matching { it.name.startsWith("ksp") }.configureEach {
-//    dependsOn("buildUniffiBindings")
-//}
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+    dependsOn("buildUniffiBindings")
+}
+
+tasks.matching { it.name.startsWith("ksp") }.configureEach {
+    dependsOn("buildUniffiBindings")
+}
---
+++ feature/biometrics/verification/src/commonMain/kotlin/com/tanda/biometrics/verification/VerificationModule.kt
@@ -0,0 +1,8 @@
+package com.tanda.biometrics.verification
+
+import org.koin.core.annotation.ComponentScan
+import org.koin.core.annotation.Module
+
+@Module
+@ComponentScan("com.tanda.biometrics.verification.repository")
+class VerificationModule
